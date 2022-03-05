b5455d5456fff34f73f4d5741f107cd7ad055b97
QuickStrike123 <73514740+QuickStrike123@users.noreply.github.com>
2022-03-05 19:30:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+    
+}
+
+function setup() {
+
+    Canvas = createCanvas(300, 300);
+
+    Canvas.center();
+    
+}
+
+function draw() {
+    
+}
+
+function TakeSnapshot() {
+
+    save("Picture.png");
+    
+}
